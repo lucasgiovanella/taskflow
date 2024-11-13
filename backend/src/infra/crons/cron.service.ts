ad776f80d49d38894fb32518568c953333f2e3c6
Lucas Giovanella <lucas.giovanella1@universo.univates.br>
2024-11-13 14:48:46 -0300

    revisão cron emails

--- backend/src/infra/crons/cron.service.ts
+++ backend/src/infra/crons/cron.service.ts
@@ -10,23 +10,38 @@ export class CronService {
 
   @Cron('0 9,14 * * 1-5')
   async handleNotifyInstalacao() {
-    const computers = await this.findComputerToNotifyUseCase.execute();
-    for (const computer of computers) {
-      const { computerName, responsible } = computer;
-      const token = process.env.EMAIL_TOKEN as string;
-      await fetch(`${process.env.EMAIL_URL || 'http://192.168.0.22:5000'}/mail-send` as string, {
-        method: 'POST',
-        body: JSON.stringify({
+    try {
+      const computers = await this.findComputerToNotifyUseCase.execute();
+
+      for (const computer of computers) {
+        const { computerName, responsible } = computer;
+
+        const data = {
           title: 'Verificar registro de instalação',
           computerName: computerName,
           body: `Caso o computador <b>${computerName}</b> esteja concluído, atualize o status do mesmo no <a href='http://192.168.0.22/installed-computers'>TaskFlow</a>`,
           to: responsible,
-          token: token,
-        }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
+          token: 'elTLSIvorZSfju3pAvHssH0Yq2eEeQDTo3kKO5t6gKalLk705S',
+        };
+
+        const emailUrl = `${
+          process.env.EMAIL_URL || 'http://192.168.0.22:5000'
+        }/mail-send`;
+
+        const response = await fetch(emailUrl, {
+          method: 'POST',
+          body: JSON.stringify(data),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error(`Erro ao enviar email: ${response.statusText}`);
+        }
+      }
+    } catch (error) {
+      console.error('Erro na execução do cron job:', error);
     }
   }
 }
